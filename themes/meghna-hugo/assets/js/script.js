7e007064b2c127de2c001a8fede6c8ec7a4591a0
Samir <gondzo@gmail.com>
2021-08-11 09:58:49 +0200

    data

--- themes/meghna-hugo/assets/js/script.js
+++ themes/meghna-hugo/assets/js/script.js
@@ -19,20 +19,26 @@ jQuery(function ($) {
 	/* ========================================================================= */
 	/*	Magnific popup
 	/* =========================================================================  */
-	$('.image-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
+	$('.gallery').each(function(){
+		$(this).magnificPopup({
+			type: 'image',
+			delegate: '.gallery-image',
+			// removalDelay: 160, //delay removal by X to allow out-animation
+			// callbacks: {
+			// 	beforeOpen: function () {
+			// 		// just a hack that adds mfp-anim class to markup
+			// 		this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+			// 		this.st.mainClass = this.st.el.attr('data-effect');
+			// 	}
+			// },
+			// closeOnContentClick: true,
+			// midClick: true,
+			// fixedContentPos: false,
+			// fixedBgPos: true,
+			gallery: {
+				enabled: true
 			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: false,
-		fixedBgPos: true
+		});
 	});
 
 	/* ========================================================================= */
